1e8b444c4c0593d40457e97bb6953f2ad4f2b077
David Berry <dberry83702@yahoo.com>
2020-01-05 19:58:05 -0600

    Adding always failing test to test out DevOps pipeline features

---
+++ DavidBerry.Framework/DavidBerry.Framework.Tests/FailingTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DavidBerry.Framework.Tests
+{
+    public class FailingTest
+    {
+
+        [Fact]
+        public void AlwaysFails()
+        {
+            Assert.True(false);
+        }
+
+    }
+}
