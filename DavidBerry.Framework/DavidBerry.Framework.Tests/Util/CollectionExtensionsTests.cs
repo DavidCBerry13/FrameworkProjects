f17e02c0a50e63281bd2fbdfd50872974b672499
David Berry <dberry83702@yahoo.com>
2020-01-10 13:26:15 -0600

    Added extension method to check if collection is null or empty

---
+++ DavidBerry.Framework/DavidBerry.Framework.Tests/Util/CollectionExtensionsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DavidBerry.Framework.Util;
+using FluentAssertions;
+using Xunit;
+
+namespace DavidBerry.Framework.Tests.Util
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void TestNullListReturnsTrueFor_IsNullOrEmpty()
+        {
+            List<string> states = null;
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeTrue();
+        }
+
+        [Fact]
+        public void TestEmptyListReturnsTrueFor_IsNullOrEmpty()
+        {
+            List<string> states = new List<string>();
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeTrue();
+        }
+
+        [Fact]
+        public void TestListWithElementsReturnsFalseFor_IsNullOrEmpty()
+        {
+            List<string> states = new List<string>() { "California", "New York", "Virginia", "Washington", "Wisconsin" };
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeFalse();
+        }
+
+        [Fact]
+        public void TestNullDictionaryReturnsTrueFor_IsNullOrEmpty()
+        {
+            Dictionary<string, string> states = null;
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void TestEmptyDictionaryReturnsTrueFor_IsNullOrEmpty()
+        {
+            Dictionary<string, string> states = new Dictionary<string, string>();
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void TestDictionaryWithElementsReturnsFalseFor_IsNullOrEmpty()
+        {
+            Dictionary<string, string> states = new Dictionary<string, string>()
+            {
+                { "WI", "Wisconsin" },
+                { "IL", "Illinois" },
+                { "MN", "Minnesota" },
+                { "MI", "Michigan" },
+                { "IA", "Iowa" },
+            };
+
+            var empty = states.IsNullOrEmpty();
+
+            empty.Should().BeFalse();
+        }
+
+    }
+}
---
+++ DavidBerry.Framework/DavidBerry.Framework/Util/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DavidBerry.Framework.Util
+{
+    public static class CollectionExtensions
+    {
+
+        /// <summary>
+        /// Convenience extension method to check if an ICollection is null or empty (i.e. has any items)
+        /// </summary>
+        /// <typeparam name="T">The generic type parameter.  This is required for the extension method definition so the method can work on any ICollection of type T</typeparam>
+        /// <param name="collection">An ICollection to check</param>
+        /// <returns>True if the collection is null or has zero elements.  False otherwise</returns>
+        public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
+        {
+            return (collection == null || collection.Count == 0);
+        }
+
+
+    }
+}
