9eaf92233d4da5ad8c2465152ee4e236c5f81bc7
David Berry <dberry83702@yahoo.com>
2025-10-23 23:24:33 -0500

    Added BusinessRuleViolationError class and updated mapping functions in ApiControllerBase (#17)

---
+++ DavidBerry.Framework/DavidBerry.Framework/Functional/BusinessRuleViolationError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidBerry.Framework.Functional
+{
+    /// <summary>
+    /// Represents an error where a business rule has been violated
+    /// </summary>
+    public class BusinessRuleViolationError : Error
+    {
+
+        public BusinessRuleViolationError(string message) : base(message)
+        {
+        }
+    }
+}
